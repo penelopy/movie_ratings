dc98a170c6dfc83bc0e3a829820cfa8ddf1b045c
Hackbright Student <info@hackbrightacademy.com>
2014-10-24 11:32:09 -0700

    movies are loading fine

--- seed.py
+++ seed.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, date
 import model
 import csv
 
@@ -38,8 +38,6 @@ def load_users(session):
 def load_movies(session):
     f = open('seed_data/u.item','r')
 
-    month_dict = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12 }
-
     lines = f.readlines()
     
     mdict = {}
@@ -48,28 +46,31 @@ def load_movies(session):
         fields = line.split('|')
         id = fields[0]
 
-        # Converting date strings to Python datetime format
-        date = fields[2].split('-')
-        print date
-        if len(date) == 3:
-            date[1] = month_dict.get(date[1],0)
-            date_obj = datetime.date(int(date[2]),int(date[1]),int(date[0]))
-        else: # if there is no date, put in default value
-            date_obj = (0,0,0)
+        # 01-Jan-1995
 
-        # Getting key error for id 1
+        datetime_obj = datetime.strptime(fields[2], "%d-%b-%Y")
+        datetime_obj = datetime_obj.date()
+
+        movie_name = fields[1]
+        movie_name = movie_name.decode("latin-1").strip().split(" ")
+        movie_name.pop()
+        movie_name = (" ").join(movie_name)
+ 
         mdict[id] = {'movie_name': fields[1],
-                    'release_date': date_obj,
+                    'release_date': datetime_obj,
                     'imdb_url': fields[3]}
+
 
     mlist = []
     for i in range(1,len(mdict)):
-        newmovie = model.Movie(movie_name= mdict[i]['movie_name'],
-                        released_at = date_obj,
-                        imdb_url = mdict[i]['imdb_url'])
+        i = str(i)
+        if i in mdict:
+            newmovie = model.Movie(movie_name= movie_name,
+                            released_at = datetime_obj,
+                            imdb_url = mdict[i]['imdb_url'])
 
         mlist.append(newmovie)
-        
+    
     for movie in mlist:
         session.add(movie)
 
@@ -85,6 +86,6 @@ def main(session):
     pass
 
 if __name__ == "__main__":
-    s= model.connect()
+    s = model.connect()
     # load_users(s)
     load_movies(s)
