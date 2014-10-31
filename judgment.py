2f32dc63f11d0d2fdb3fc49c45007cb195abf054
Hackbright Student <info@hackbrightacademy.com>
2014-10-30 17:53:51 -0700

    Fixed url bugs. Code review by Sean.

--- judgment.py
+++ judgment.py
@@ -6,13 +6,15 @@ app = Flask(__name__)
 
 app.secret_key = 'a4c96d59-57a8-11e4-8b97-80e6500ee2f6'
 
+#Run when browser is refreshed and sets g for id and email of the user that is logged in
 @app.before_request
 def check_login():
-    user_data = flask_session.get('user')
+    user_data = flask_session.get('user') 
     if user_data and len(user_data) > 1:
         g.user_id = user_data[0]
         g.user_email = user_data[1]
 
+#Home page view. Queries database for 15 users and displays the number of ratings they have done
 @app.route("/")
 def index():
     # Replace with cover page later
@@ -28,13 +30,12 @@ def signin():
 def login():
     email = request.form['email']
     password = request.form['password']
-
-    user = model.session.query(model.User).filter_by(email=email).filter_by(password=password).first()
+    #A Database query that looks in the User table for the email and password and it return the user object/instance
+    user = model.session.query(model.User).filter_by(email=email).filter_by(password=password).one()
 
     if user:
-        flask_session['user'] = [user.id, user.email]
+        flask_session['user'] = [user.id, user.email] #adds the current user to the flask session
         flash("Login successful.")
-        # url = "/user?user=" + str(user.id) ## Do away with this and use route below
         return redirect(url_for('user_reviews', user_id=user.id))         
     else:
         flash("Username/password is invalid")
@@ -42,55 +43,24 @@ def login():
 
 @app.route("/logout", methods=['GET','POST'])
 def logout():
-    flask_session['user'] = []
+    flask_session['user'] = [] #clears the user key stored in the flask session
     flash("You are logged out.")
     return redirect(url_for('index'))
-
-# @app.route()
-    ## Make a page and route that goes to the current user's ratings / account info
-    
-
-# HTML
-# <a href="/movie/{{movie.id}}">link</a>
-# evaluates to <a href="/movie/4">link</a>"
-
-# @app.route("/movie/<int:id>")
-# def route_name(id):
-#     request.args.get("id")
-#     do some stuff
 
 
 @app.route('/user/<int:user_id>')
 def user_reviews(user_id):
-    # user_id = request.args.get("id")
-    user = model.session.query(model.User).get(user_id) # User
-    user_rating_dict = {}
-
-    for rating in user.ratings:
-        movie_rating = rating.rating
-        movie_name = rating.movie.movie_name
-        movie_id = rating.movie.id
-        user_rating_dict[rating.movie_id] = [movie_name, movie_rating, movie_id]
-
-    return render_template("user_rating.html", reviews=user_rating_dict, user=user.id)
+    #Queries the User table by user_id and returns a user object/instance
+    user_obj = model.session.query(model.User).get(user_id) # User
+    return render_template("user_rating.html", ratings=user_obj.ratings, user=user_obj.id)
 
 @app.route('/movie/<int:movie_id>')
 def movie_reviews(movie_id):
-    # movie_id = request.args.get("id")
-    # movie_id = int(movie_id)
-    movie = model.session.query(model.Movie).get(movie_id) # Movie    
-    movie_rating_dict = {}
+    movie_obj = model.session.query(model.Movie).get(movie_id) # Movie    
+    return render_template("movie_ratings.html", movie=movie_obj)
 
-    for rating in movie.ratings:               
-        movie_rating = rating.rating
-        user_id = rating.user_id 
-        movie_rating_dict[user_id] = movie_rating
-
-    return render_template("movie_ratings.html", reviews=movie_rating_dict, movie=movie)
-
-@app.route('/new_review', methods=['POST'])
-def add_review():
-    movie_id = request.args.get("movie")
+@app.route('/new_review/<int:movie_id>', methods=['POST'])
+def add_review(movie_id):
     user_id = flask_session['user'][0]
     rating = request.form['rating']
 
@@ -100,22 +70,17 @@ def add_review():
     model.session.add(new_review)
     model.session.commit()
 
-    url = '/movie?movie=' + movie_rating_dict   # Need to redo this url
-    return redirect(url)
+    return redirect(url_for('account_page'))
 
 
 @app.route('/update_review/<int:movie_id>', methods=['GET','POST'])
 def update_review(movie_id):
-    # receive rating
-    # and set new new rating
-    # return new rating
-
-    # user_id.rating = new_rating
     m = movie_id
     u = flask_session['user'][0]
-    r = request.form['rating']
+    r = request.form.get('rating')
 
-    model.Rating.update().values(rating=r).where(user_id = u).where(movie_id=m)                 
+    rating_obj = model.session.query(model.Rating).filter_by(user_id = u).filter_by(movie_id = m).one()                
+    rating_obj.rating = r #changes the old rating from the database to the new rating -"r"
     model.session.commit()
     flash("Review updated.")
     return redirect("/myaccount")
@@ -123,16 +88,8 @@ def update_review(movie_id):
 @app.route('/myaccount')
 def account_page():
     user_id = flask_session['user'][0]
-    user = model.session.query(model.User).get(user_id) # User
-    user_rating_dict = {}
-
-    for rating in user.ratings:
-        movie_rating = rating.rating
-        movie_name = rating.movie.movie_name
-        movie_id = rating.movie.id
-        user_rating_dict[rating.movie_id] = [movie_name, movie_rating, movie_id]
-
-    return render_template("your_ratings_page.html",reviews=user_rating_dict, user=user.id )
+    user_obj = model.session.query(model.User).get(user_id) # User
+    return render_template("your_ratings_page.html", ratings=user_obj.ratings, user=user_obj.id)
 
 @app.route("/registration")
 def registration_form():
@@ -141,7 +98,7 @@ def registration_form():
 
 @app.route("/newuser", methods=['POST'])
 def add_new_user():
-
+    #request.form takes the information submitted in the form on registration.html
     email = request.form['email']
     password = request.form['password']
     age = request.form['age']
--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import datetime
+import correlation
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy import Column, Integer, String, DateTime, update
@@ -7,7 +8,7 @@ from sqlalchemy.orm import sessionmaker, relationship, backref, scoped_session
 # # ENGINE = None
 # Session = None
 
-ENGINE = create_engine("sqlite:///ratings.db", echo=False)
+ENGINE = create_engine("sqlite:///ratings.db", echo=False) 
 session = scoped_session(sessionmaker(bind=ENGINE, autocommit = False, autoflush = False))
 
 Base = declarative_base()
@@ -27,6 +28,40 @@ class User(Base):
     gender = Column(String(15), nullable = True)
     occupation = Column(String(15), nullable = True)
 
+    # also has ratings attribute
+
+    def similarity(self,other):
+        u_ratings = {}
+        paired_ratings = []
+
+        for r in self.ratings:
+            u_ratings[r.movie_id] = r
+
+        for r in other.ratings:
+            u_r = u_ratings.get(r.movie_id)
+            if u_r:
+                paired_ratings.append( (u_r.rating, r.rating) )
+
+        if paired_ratings:
+            return correlation.pearson(paired_ratings)
+        else:
+            return 0.0
+
+    def predict_rating(self, movie):
+        ratings = self.ratings
+        other_ratings = movie.ratings
+        other_users = [ r.user for r in other_ratings ]
+        similarities = [ (self.similarity(other_user), other_user) \
+            for other_user in other_users ]
+        similarities.sort(reverse = True)
+        top_user = similarities[0]
+        matched_rating = None
+        for rating in other_ratings:
+            if rating.user_id == top_user[1].id:
+                matched_rating = rating
+                break
+        return matched_rating.rating * top_user[0]
+
 class Movie(Base):
     __tablename__ = "movies"
 
@@ -35,7 +70,7 @@ class Movie(Base):
     release_year = Column(DateTime, nullable = True)
     imdb_url = Column(String(64), nullable = True)
 
-    # has ratings attribute
+    # also has ratings attribute
 
 class Rating(Base):
     __tablename__ = "ratings"
@@ -45,12 +80,13 @@ class Rating(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
     rating = Column(Integer, nullable = True)
 
-    user = relationship("User", backref=backref("ratings", order_by=id))
-    movie = relationship("Movie", backref=backref("ratings", order_by=id))
+    user = relationship("User", backref=backref("ratings", order_by=id)) #this sets relationship between user and ratings
+    movie = relationship("Movie", backref=backref("ratings", order_by=id)) #this sets relationship between movie and ratings
 
 def create_tables():
     engine = create_engine("sqlite:///ratings.db", echo=True)
     Base.metadata.create_all(engine)
+
 
 ### End class declarations
 
